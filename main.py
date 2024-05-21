af6a270f9ba968ea7e50eecffd17e8f6f4f038e0
yonice7 <jvergara041@gmail.com>
2024-05-21 16:26:20 -0300

    adding a todo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 import asyncio
 import os
+# from .merc import login
+from anelu.login import login_anelu
 
 
 def main():
@@ -8,4 +10,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(login_anelu())
+    # TODO: If function returns successful run the invoices
